d84e99954dfbf7917e9f71c928acc9e76273485a
Tatsiana Putryk <tushkanchik85@gmail.com>
2023-02-18 15:05:50 +0300

    Task_03 added

---
+++ Task_03/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27        5 -> 1, 8, 27, 64, 125
+
+int InputInt(string message)
+{
+    Console.Write($"{message} ");
+    int value;
+    if(int.TryParse(Console.ReadLine(), out value))
+    {
+        return value;
+    }
+    Console.WriteLine("Вы ввели не число");
+    Environment.Exit(1);
+    return 0;
+}
+
+int number = InputInt("Введите число N, для которого выдадим таблицу кубов чисел от 1 до N ");
+
+for(int i = 1; i <= number; i++)
+{
+    int cube = i * i * i;
+    Console.WriteLine($"{cube}");
+}
